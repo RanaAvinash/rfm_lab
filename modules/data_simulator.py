e6267df83b3d64d812ac21ffde37e393cd512dc8
RanaAvinash <140587865+RanaAvinash@users.noreply.github.com>
2026-03-11 11:42:50 +0530

    Update data_simulator.py

--- modules/data_simulator.py
+++ modules/data_simulator.py
@@ -1,1 +1,24 @@
+import pandas as pd
+import numpy as np
 
+def simulate_data(customers,transactions):
+
+    np.random.seed(42)
+
+    df = pd.DataFrame({
+
+        "CustomerID":np.random.randint(10000,10000+customers,transactions),
+
+        "InvoiceNo":np.random.randint(100000,400000,transactions),
+
+        "InvoiceDate":pd.date_range("2024-01-01",periods=transactions),
+
+        "Quantity":np.random.randint(1,5,transactions),
+
+        "UnitPrice":np.random.uniform(5,50,transactions)
+
+    })
+
+    df["Sales"] = df["Quantity"] * df["UnitPrice"]
+
+    return df
